c3e6d9f4faa89f3416729b6cd79a77d096ab8c05
codefactor-io <support@codefactor.io>
2019-10-04 22:29:20 +0000

    [CodeFactor] Apply fixes to commit b3dcd61
    [ci skip] [skip ci]

--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
-import TodoItem from './TodoItem';
 import PropTypes from 'prop-types';
+import TodoItem from './TodoItem';
 
 class Todos extends Component {
   render() {
-    return this.props.todos.map(todo =>
+    return this.props.todos.map((todo) => (
       <TodoItem
         key={todo.id}
         todo={todo}
         markComplete={this.props.markComplete}
         deleteTodo={this.props.deleteTodo}
       />
-    );
+));
   }
 }
 
 Todos.propTypes = {
   todos: PropTypes.array.isRequired,
   markComplete: PropTypes.func.isRequired,
-  deleteTodo: PropTypes.func.isRequired
-}
+  deleteTodo: PropTypes.func.isRequired,
+};
 
 export default Todos;
