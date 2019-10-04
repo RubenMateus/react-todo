7d850da2d3454ea2e4a36f9c63234fceee0e898b
Restyled.io <commits@restyled.io>
2019-10-04 23:39:42 +0000

    Restyled by prettier

--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -4,7 +4,7 @@ import TodoItem from "./TodoItem";
 
 class Todos extends Component {
   render() {
-    return this.props.todos.map((todo) => (
+    return this.props.todos.map(todo => (
       <TodoItem
         key={todo.id}
         todo={todo}
