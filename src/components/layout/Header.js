0c3bdff9e338555974009dfed2e3f73b9c7fe2e0
Ruben Mateus <ruben.mateus3@gmail.com>
2019-11-03 21:39:47 +0000

    update build

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { FaPizzaSlice } from "react-icons/fa";
 
 export const Header = () => (
-  <header className="header" data-testid="header" style={headerStyle}>
+  <header className="header" data-testid="header">
     <nav>
       <div className="logo">
-        <img src="/images/logo.png" alt="Todoist" />
+        <img src="/images/logo.png" alt="Todo App" />
       </div>
       <div className="settings">
         <ul>
