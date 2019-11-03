bf7e233493637f2df64085647e630e7d7f530500
Ruben Mateus <ruben.mateus3@gmail.com>
2019-11-03 01:28:08 +0000

    minor updates in hooks and configs for husky eslint

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { firebase } from '../firebase';
-import { collatedTasksExist } from '../helpers'
+import { collatedTasksExist } from '../helpers';
 import moment from 'moment';
 
 export const useTasks = selectProject => {
@@ -13,31 +13,65 @@ export const useTasks = selectProject => {
       .collection('tasks')
       .where('userId', '==', 'ruben');
 
-    unsubscribe = selectProject && !collatedTasksExist(selectProject) ?
-      (unsubscribe = unsubscribe.where('projectId', '==', selectProject)) : selectProject === 'TODAY' ?
-      (unsubscribe = unsubscribe.where('date', '==', moment().format('DD/MM/YYYY'))) :
-      selectProject === 'INBOX' || selectProject === 0 ?
-      (unsubscribe = unsubscribe.where('date', '==', '')) :
-      unsubscribe;
+    unsubscribe =
+      selectProject && !collatedTasksExist(selectProject)
+        ? (unsubscribe = unsubscribe.where('projectId', '==', selectProject))
+        : selectProject === 'TODAY'
+        ? (unsubscribe = unsubscribe.where(
+            'date',
+            '==',
+            moment().format('DD/MM/YYYY')
+          ))
+        : selectProject === 'INBOX' || selectProject === 0
+        ? (unsubscribe = unsubscribe.where('date', '==', ''))
+        : unsubscribe;
 
     unsubscribe = unsubscribe.onSnapshot(snapshot => {
       const newTasks = snapshot.docs.map(task => ({
         id: task.id,
-        ...task.data(),
+        ...task.data()
       }));
 
       setTasks(
-        selectProject === 'NEXT_7_DAYS' ?
-          newTasks.filter(task => moment(task.date, 'DD-MM-YYYY').diff(moment(), 'days') <= 7 && !task.archived) :
-          newTasks.filter(task => !task.archived)
+        selectProject === 'NEXT_7_DAYS'
+          ? newTasks.filter(
+              task =>
+                moment(task.date, 'DD-MM-YYYY').diff(moment(), 'days') <= 7 &&
+                !task.archived
+            )
+          : newTasks.filter(task => !task.archived)
       );
 
-      setArchivedTasks(newTasks.filter(task => task.archived))
+      setArchivedTasks(newTasks.filter(task => task.archived));
     });
 
     return () => unsubscribe();
-  }, [selectProject])
+  }, [selectProject]);
 
   return { tasks, archivedTasks };
-}
+};
 
+export const useProjects = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection('projects')
+      .where('userId', '==', 'ruben')
+      .orderBy('projectId')
+      .get()
+      .then(snapshot => {
+        const allProjects = snapshot.docs.map(project => ({
+          ...project.data(),
+          docId: project.id
+        }));
+
+        if (JSON.stringify(allProjects) !== JSON.stringify(projects)) {
+          setProjects(allProjects);
+        }
+      });
+  }, [projects]);
+
+  return { projects, setProjects };
+};
